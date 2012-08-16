44972eeee85bfe1afc59a8d46ea6a97cd9a85f1e
Sam Holman <sam@labelmedia.co.uk>
2012-08-16 10:17:09 +0100

    Adding comment block to test bootstrap.

--- tests/Bootstrap.inc.php
+++ tests/Bootstrap.inc.php
@@ -1,3 +1,14 @@
 <?php
 
+namespace Yetti\API\Tests;
+
+/**
+ * Test suite bootstrap.
+ *
+ * @author Sam Holman <sam@yetti.co.uk>
+ * @copyright Copyright (c) 2011-2012, Yetti Ltd.
+ * @package yetti-api
+ * @subpackage tests
+ */
+
 require_once 'api/Webservice.inc.php';
