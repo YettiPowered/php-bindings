0b7110ea8a3429b88ad389abf77e911e815f78fa
Sam Holman <sam@labelmedia.co.uk>
2012-08-16 09:15:34 +0100

    Moving proxy file.

--- tools/proxy.php
+++ tools/proxy.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'api/Webservice.inc.php';
+require_once '../api/Webservice.inc.php';
 
 $accessKey   	= isset($_POST['accessKey']) ? $_POST['accessKey'] : null;
 $privateKey  	= isset($_POST['privateKey']) ? $_POST['privateKey'] : null;
