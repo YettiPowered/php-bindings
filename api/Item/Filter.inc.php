9e520e962d036c885c3602d291872660abc34daa
Sam Holman <sam@labelmedia.co.uk>
2012-08-20 10:05:44 +0100

    Adding functionality to filter model to add conditional filters, filtered items and filter deletion. Plus test methods for each of these.

--- api/Item/Filter.inc.php
+++ api/Item/Filter.inc.php
@@ -29,6 +29,7 @@ class Item_Filter extends BaseAbstract
 		if ($this->webservice()->makeRequest())
 		{
 			$this->setJson($this->webservice()->getResponseJsonObject());
+			$this->_filterTypeId = $this->getJson()->filterTypeId;
 			return true;
 		}
 		
@@ -54,11 +55,23 @@ class Item_Filter extends BaseAbstract
 		$this->webservice()->setPostData(json_encode($this->getJson()));
 		$result = $this->makeRequestReturnResult();
 		
-		if ($result->success()) {
+		if ($result->success() && !$this->getId()) {
 			$this->getJson()->id = (int)$this->webservice()->getResponseHeader('X-ResourceId');
 		}
 		
 		return $result;
+	}
+	
+	/**
+	 * Delete this filter
+	 * 
+	 * @return \Yetti\API\Result
+	 */
+	public function delete()
+	{
+		$this->webservice()->setRequestPath('/items/filters/null/null/' . $this->getId() . '.ws');
+		$this->webservice()->setRequestMethod('delete');
+		return $this->makeRequestReturnResult();
 	}
 	
 	/**
@@ -72,6 +85,16 @@ class Item_Filter extends BaseAbstract
 		if (is_numeric($id)) {
 			$this->_filterTypeId = (int)$id;
 		}
+	}
+	
+	/**
+	 * Return the filter type ID
+	 * 
+	 * @return int
+	 */
+	public function getFilterTypeId()
+	{
+		return $this->_filterTypeId;
 	}
 	
 	/**
@@ -106,6 +129,52 @@ class Item_Filter extends BaseAbstract
 	}
 	
 	/**
+	 * Add a conditional filter by ID
+	 * 
+	 * @param int $filterId
+	 * @return void
+	 */
+	public function addConditionalFilter($filterId)
+	{
+		if (is_numeric($filterId)) {
+			$this->getJson()->conditionalFilters[] = (int)$filterId;
+		}
+	}
+	
+	/**
+	 * Returns an array of conditional filter IDs
+	 * 
+	 * @return array
+	 */
+	public function getConditionalFilters()
+	{
+		return $this->getJson()->conditionalFilters;
+	}
+	
+	/**
+	 * Add an item to this filter
+	 * 
+	 * @param int $itemId
+	 * @return void
+	 */
+	public function addItem($itemId)
+	{
+		if (is_numeric($itemId)) {
+			$this->getJson()->items[] = (int)$itemId;
+		}
+	}
+	
+	/**
+	 * Returns an array of items in this filter
+	 * 
+	 * @return array
+	 */
+	public function getItems()
+	{
+		return $this->getJson()->items;
+	}
+	
+	/**
 	 * Returns the json object
 	 * 
 	 * @return \stdClass
@@ -118,8 +187,9 @@ class Item_Filter extends BaseAbstract
 		catch (\yetti\API\Exception $e)
 		{
 			$template = new \stdClass();
-			$template->id   = null;
-			$template->name = null;
+			$template->id    = null;
+			$template->name  = null;
+			$template->items = array();
 			$template->conditionalFilters = array();
 			
 			$this->setJson($template);
--- tests/Item/FilterTest.php
+++ tests/Item/FilterTest.php
@@ -64,5 +64,72 @@ class Item_FilterTest extends AuthAbstract
 		$filter = new \Yetti\API\Item_Filter();
 		$this->assertTrue($filter->load($filterId));
 		$this->assertEquals('Changed', $filter->getName());
+		
+		return $filter;
+	}
+	
+	/**
+	 * @depends testUpdate
+	 */
+	public function testConditionalFilter(\Yetti\API\Item_Filter $inFilter)
+	{
+		$filter = new \Yetti\API\Item_Filter();
+		$filter->setFilterTypeId($inFilter->getFilterTypeId());
+		$filter->setName('Conditional filter');
+		$filter->addConditionalFilter($inFilter->getId());
+		$this->assertTrue($filter->save()->success());
+		$filterId = $filter->getId();
+		
+		$filter = new \Yetti\API\Item_Filter();
+		$this->assertTrue($filter->load($filterId));
+		
+		$conditionalFilters = $filter->getConditionalFilters();
+		$this->assertCount(1, $conditionalFilters);
+		$this->assertEquals($inFilter->getId(), $conditionalFilters[0]);
+		
+		return $filter;
+	}
+	
+	/**
+	 * @depends testConditionalFilter
+	 */
+	public function testAssignItems(\Yetti\API\Item_Filter $inFilter)
+	{
+		/**
+		 * Set up an item first...
+		 */
+		$item = new \Yetti\API\Item();
+		$this->assertTrue($item->loadTemplate(4));
+		$item->setName('A test item ' . microtime(true));
+		$item->setPropertyValue('Name', 'Test item');
+		$item->setPropertyValue('Body', 'A test body');
+		$this->assertTrue($item->save()->success());
+		
+		$this->assertCount(0, $inFilter->getItems());
+		$inFilter->addItem($item->getId());
+		$this->assertCount(1, $inFilter->getItems());
+		$this->assertTrue($inFilter->save()->success());
+		
+		$filter = new \Yetti\API\Item_Filter();
+		$this->assertTrue($filter->load($inFilter->getId()));
+		$this->assertCount(1, $filter->getItems());
+		
+		return $filter;
+	}
+	
+	/**
+	 * @depends testAssignItems
+	 */
+	public function testDelete(\Yetti\API\Item_Filter $inFilter)
+	{
+		$filterId = $inFilter->getId();
+		
+		$filter = new \Yetti\API\Item_Filter();
+		$this->assertTrue($filter->load($filterId));
+		
+		$this->assertTrue($filter->delete()->success());
+		
+		$filter = new \Yetti\API\Item_Filter();
+		$this->assertFalse($filter->load($filterId));
 	}
 }
